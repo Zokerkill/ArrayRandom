d2ce93074576c546b2c49c2083bf0d63124172de
pupil <pupil@ivkhk.ee>
2019-02-28 11:13:22 +0200

    Рандомные числа

---
+++ src/arrayrandom/ArrayRandom.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayrandom;
+
+import classes.App;
+/**
+ *
+ * @author pupil
+ */
+public class ArrayRandom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App();
+        
+    }
+    
+}
